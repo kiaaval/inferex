f9b4dce8b76fb714191f9726fba3f96c8ad80beb
Kiamehr <kiamehraval@gmail.com>
2026-05-04 13:00:00 -0700

    Extend proposition types for inference

--- types.ts
+++ types.ts
@@ -1,23 +1,30 @@
 
+type figure = 1 | 2 | 3 | 4;
 type propType = "A" | "E" | "I" | "O";
 type role = "major" | "minor";
 type quantity = "all" | "some" | "no" | "singular";
 type quality = true | false;
 type propositionKey = "all-true" | "no-true" | "some-true" | "some-false" | "all-false" | "no-false" | "singular-true" | "singular-false";
 
-type syllogism = {
+type qualityText = `${boolean}-${boolean}`;
+type qualityTextTypes = "is" | "are" | "is not" | "are not";
+
+type lines = {
     lineOne: string;
     lineTwo: string;
 }
 
 type proposition = {
     propType: propType;
-    role?: role;
+    role?: role | undefined;
     quantity: quantity;
     quality: quality;
     subject: string;
     predicate: string;
 }
 
+type termOrder = "m-p" | "p-m" | "m-s" | "s-m";
+type figureOrder = "m-p,s-m" | "p-m,s-m" | "m-p,m-s" | "p-m,m-s";
+type mood = `${propType}${propType}-${figure}`;
 
-export type {proposition, syllogism, propType, propositionKey, quantity, quality };
+export type {proposition, lines, propType, propositionKey, quantity, quality, role, termOrder, figureOrder, figure, mood, qualityText, qualityTextTypes };
