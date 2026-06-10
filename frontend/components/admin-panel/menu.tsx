0e1eb03b0580186f43efe1ba1fbca6a0d4360132
Kiamehr <kiamehraval@gmail.com>
2026-06-09 17:54:41 -0700

    Open account menu upward

--- frontend/components/admin-panel/menu.tsx
+++ frontend/components/admin-panel/menu.tsx
@@ -153,7 +153,7 @@ export function Menu({ isOpen }: MenuProps) {
                     Account
                   </p>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent side="right" sideOffset={12} align="end" className="w-60">
+                <DropdownMenuContent side="top" sideOffset={12} align="end" className="w-60">
                   <DropdownMenuLabel className="font-normal">
                     <div className="flex flex-col space-y-1">
                       <p className="truncate text-sm font-medium leading-none">{user.name}</p>
